123b18a4c8e67cb415296f3a6fddfebd632603b8
Franknkiwani <132098616+Franknkiwani@users.noreply.github.com>
2025-07-06 19:40:07 +0200

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+// firebase-messaging-sw.js
+
+importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDFHskUWiyHhZke3KT9kkOtFI_gPsKfiGo",
+  authDomain: "itzhoyoo-f9f7e.firebaseapp.com",
+  projectId: "itzhoyoo-f9f7e",
+  messagingSenderId: "1094792075584",
+  appId: "1:1094792075584:web:d49e9c3f899d3cd31082a5"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Background message received:', payload);
+
+  const notificationTitle = payload.notification?.title || "New Notification";
+  const notificationOptions = {
+    body: payload.notification?.body || '',
+    icon: '/icons/icon-192x192.png' // optional, but good to have
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
